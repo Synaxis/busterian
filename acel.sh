b252f0ee3be055e995fe108290dd7e5ae06712bc
Synaxis <thecaiotor@gmail.com>
2023-07-15 15:16:34 -0300

    Create acel.sh

---
+++ acel.sh
@@ -0,0 +1,3 @@
+xinput --set-prop 'pointer:''Logitech G300s Optical Gaming Mouse' 'libinput Accel Profile Enabled' 0, 1
+
+xinput --set-prop 'pointer:''Logitech G300s Optical Gaming Mouse' 'libinput Accel Speed' -0.1
