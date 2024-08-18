74d9d458bf14f1a9513060a54479f464a5ce4097
Caio A. Torquato <01xtor@gmail.com>
2024-08-18 14:55:19 -0300

    Update configModern.nix

--- nixos/configModern.nix
+++ nixos/configModern.nix
@@ -80,7 +80,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # System packages
