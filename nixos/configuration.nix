82d2d73d70e6f4cf454a0fb19f9845537a7ec953
Caio A. Torquato <01xtor@gmail.com>
2024-08-18 14:10:41 -0300

    Fix hostname issue

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,23 +10,17 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Valid hostname.
+  networking.hostName = "nixos-3839j"; # Change this to a valid hostname
 
-  networking.hostName = "nixos_#3839j"; # hostname to be found on network like smb 
-  # networking.wireless.enable = true;  # enable wireless wpa_supplicant.
-
-  # Configure o proxy de rede, se necessário
-  # networking.proxy.default = "http://usuario:senha@proxy:porta/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Habilitar rede
+  # Enable network manager
   networking.networkmanager.enable = true;
 
-  # Defina seu fuso horário.
+  # Set timezone
   time.timeZone = "America/Sao_Paulo";
 
-  # Selecione as propriedades de internacionalização.
+  # Internationalization
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
@@ -39,17 +33,15 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Configurar layout de teclado no X11
+  # Keyboard layout
   services.xserver.layout = "br";
   services.xserver.xkbVariant = "";
-
-  # Configurar layout de teclado no console
   console.keyMap = "br-abnt2";
 
-  # Habilitar CUPS para imprimir documentos.
+  # Enable CUPS
   services.printing.enable = true;
 
-  # Habilitar som com pipewire.
+  # Audio
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -57,15 +49,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # Se você quiser usar aplicativos JACK, descomente isso
-    # jack.enable = true;
-
-    # media-session.enable = true;
   };
 
-  # suport  touchpad
-  # services.xserver.libinput.enable = true;
-
+  # User configuration
   users.users.metaorior = {
     isNormalUser = true;
     description = "metaorior";
@@ -78,48 +64,26 @@
   services.xserver.displayManager.lightdm.autoLogin.enable = true;
   services.xserver.displayManager.lightdm.autoLogin.user = "metaorior";
 
-  # Instalar firefox.
+  # Optional Firefox installation
   programs.firefox.enable = false;
 
-  # Permitir pacotes não livres
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-
+  # NVIDIA driver configuration
   nixpkgs.config.nvidia.acceptLicense = true;
-  # NVIDIA driver
   services.xserver.videoDrivers = [ "nvidia" ];
-
-  # Habilitar decodificação de vídeo acelerada por hardware para alguns aplicativos
   hardware.opengl.enable = true;
-
   hardware.nvidia = {
-
-    # O modesetting é necessário.
     modesetting.enable = true;
-
-    # Gerenciamento de energia Nvidia. Experimental, e pode fazer com que o modo de espera/suspensão falhe.
-    # Habilite isso se você tiver problemas de corrupção gráfica ou travamentos de aplicativos após acordar
-    # do modo de espera. Isso corrige ao salvar toda a memória VRAM em /tmp/ 
-    # em vez de apenas o essencial.
     powerManagement.enable = false;
-
-    # Gerenciamento de energia detalhado. Desliga a GPU quando não está em uso.
-    # Experimental e só funciona em GPUs Nvidia modernas (Turing ou mais recentes).
     powerManagement.finegrained = false;
-
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Disponível apenas a partir do driver 515.43.04+
     open = false;
-
-    # nvidia-settings
     nvidiaSettings = true;
-
-    # here i use 470 because my gpu is an old tesla gt 730 low profile
     package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
   };
 
-  # Liste os pacotes instalados no perfil do sistema. Para buscar, execute:
-  # $ nix search wget
+  # System packages
   environment.systemPackages = with pkgs; [
     unzip
     zip
@@ -143,40 +107,18 @@
     rar
     unrar
     python3
-    bluez
     git
-    bluez-alsa
     gnome.ghex
     gnome.gnome-sudoku
     mesa
     libglvnd
-    libglvnd.dev
-    mesa
   ];
 
-  # Alguns programas precisam de wrappers SUID, podem ser configurados ainda mais ou são
-  # iniciados em sessões de usuário.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Enable SDDM and Plasma5
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.enable = true;
 
-  # Liste os serviços que você deseja habilitar:
-
-  # Habilitar o daemon OpenSSH.
-  # services.openssh.enable = true;
-
-  # firewall ports
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Ou desabilitar o firewall completamente.
-  # networking.firewall.enable = false;
-
-  #set nixos version
+  # Set NixOS version
   system.stateVersion = "24.05";
-
-services.xserver.displayManager.sddm.enable = true;
-services.xserver.desktopManager.plasma5.enable = true;
-services.xserver.enable = true;
 }
