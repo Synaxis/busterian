2ae5829685c15f7b7232c68e94f6e4c9bb8e2cbe
Caio A. Torquato <01xtor@gmail.com>
2024-09-06 01:01:09 -0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 
 {
   imports =
-    [ # default
+    [ 
       ./hardware-configuration.nix
     ];
 
@@ -11,7 +11,15 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Valid hostname
-  networking.hostName = "nixos-3839j"; # Change this to a valid hostname
+  networking.hostName = "nixos-3839j"; 
+
+services.mysql = {
+  enable = true;
+  package = pkgs.mysql;
+
+
+};
+
 
   # Enable NTFS support at boot
   boot.supportedFilesystems = [ "ntfs" ];
@@ -43,8 +51,11 @@
 
   # Enable CUPS
   services.printing.enable = true;
+hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.driSupport = true;
 
-  # Audio
+
+  # Audio setup with PipeWire
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -58,24 +69,27 @@
   users.users.metaorior = {
     isNormalUser = true;
     description = "metaorior";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "adbusers" "kvm" ]; # Added adbusers and kvm for Android and Emulator
+
+
     packages = with pkgs; [
       xfce.xfce4-terminal
     ];
   };
 
+  # Optional auto-login setup
   services.xserver.displayManager.lightdm.autoLogin.enable = true;
   services.xserver.displayManager.lightdm.autoLogin.user = "metaorior";
 
   # Optional Firefox installation
   programs.firefox.enable = false;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+networking.firewall.enable = false;
 
   # NVIDIA driver configuration
+    nixpkgs.config.android_sdk.accept_license = true;
   nixpkgs.config.nvidia.acceptLicense = true;
   services.xserver.videoDrivers = [ "nvidia" ];
+  
   hardware.opengl.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
@@ -86,7 +100,7 @@
     package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
   };
 
-  # System packages
+  # System packages and Python configuration to allow pip usage
   environment.systemPackages = with pkgs; [
     unzip
     zip
@@ -96,29 +110,91 @@
     kate
     google-chrome
     vlc
-    libreoffice
-    gzipv
+    gcc
+    dxvk
+    volk
+vulkan-headers
+gpt4all
+gst_all_1.gstreamer
+tor-browser
+   libreoffice
+    gzip
     gimp
     htop
+    llvmPackages_12.libcxx
+    wireshark
+    libcxx
+
     neofetch
     handbrake
+    xorg_sys_opengl
+    zenith-nvidia
     libgcc
+    android-tools
+    obs-studio
     virtualbox
+    wget
+    ffmpeg-full
+    handbrake
+    autoconf
+    gnumake
+    automake
+    wine
+    ninja
+    driversi686Linux.libvdpau-va-gl
+    driversi686Linux.vaapiVdpau
+    driversi686Linux.mesa
+    vulkan-volk
+    sdkmanager
+    vulkan-utility-libraries
+    winetricks
     wineWowPackages.full
     blueman
-    jdk22
-python312Packages.pip
+    steam-run
+    cmake
+    nvidia-vaapi-driver
+    jdk19
+        android-tools
+    androidenv.androidPkgs_9_0.androidsdk    # Add Android SDK package
+    androidenv.androidPkgs_9_0.platform-tools # Add platform tools
+    vulkan-tools
     prismlauncher
+    libgcc
+    python312Packages.tkinter
     rar
-gedit
+    gedit
+    flutter
+    dbeaver-bin
+    libglvnd
     unrar
-    python3
+    mysql-workbench
+        lutris
+    pipx
+    cabextract
+    python3Full
+    python312Packages.pip  # Ensure pip is included
     git
     gnome.ghex
     gnome.gnome-sudoku
     mesa
     libglvnd
+    (python3.withPackages (ps: with ps; [ pip moviepy ]))  # Python with pip and MoviePy
   ];
+
+  # Nix configuration to bypass restrictions (use with caution)
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowBroken = true;  # Allow broken packages if necessary
+    allowInsecure = true;  # Allows the use of insecure packages
+  };
+  
+    # Enable adb for unprivileged users
+  programs.adb.enable = true;
+  services.udev.packages = [ pkgs.android-udev-rules ];
+
+
+  # Enable pip to work outside of virtual environments
+  environment.variables.PIP_REQUIRE_VIRTUALENV = "false";
 
   # Enable SDDM and Plasma5
   services.xserver.displayManager.sddm.enable = true;
